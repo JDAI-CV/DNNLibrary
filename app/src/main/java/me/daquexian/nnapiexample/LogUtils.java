df1cc28dbb7833994099497f687bf5eb55ac1872
daquexian <daquexian566@gmail.com>
2017-11-07 17:18:50 +0800

    Add LogUtils

---
+++ app/src/main/java/me/daquexian/nnapiexample/LogUtils.java
@@ -0,0 +1,19 @@
+package me.daquexian.nnapiexample;
+
+
+import android.util.Log;
+
+public class LogUtils {
+    public static void d(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+    public static void w(String tag, String msg) {
+        Log.w(tag, msg);
+    }
+    public static void e(String tag, String msg) {
+        Log.e(tag, msg);
+    }
+    public static void i(String tag, String msg) {
+        Log.i(tag, msg);
+    }
+}
