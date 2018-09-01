5cb6adc14a6087a38c23833a96ffa52c438ae0cd
daquexian <daquexian566@gmail.com>
2018-09-01 19:20:04 +0800

    Update onnx sudmodule and onnx.cmake

--- cmake/onnx.cmake
+++ cmake/onnx.cmake
@@ -1,9 +1,11 @@
 macro(configure_onnx)
     message(STATUS "Configuring onnx...")
     set(DAQ_ONNX_NAMESPACE onnx_daq)
+    
     set(ONNX_NAMESPACE ${DAQ_ONNX_NAMESPACE})
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/onnx)
-    target_compile_definitions(onnx
-            PUBLIC
-            -DONNX_NAMESPACE=${DAQ_ONNX_NAMESPACE})
+    # Since https://github.com/onnx/onnx/pull/1318 is merged, we don't need to set it manually
+    # target_compile_definitions(onnx
+    # PUBLIC
+    # -DONNX_NAMESPACE=${DAQ_ONNX_NAMESPACE})
 endmacro()
