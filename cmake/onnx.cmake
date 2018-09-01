2dac5575d70bbb1bc89a2550b4f36ac1c94eb55c
daquexian <daquexian566@gmail.com>
2018-09-01 23:09:33 +0800

    Fix onnx namespace

--- cmake/onnx.cmake
+++ cmake/onnx.cmake
@@ -2,7 +2,7 @@ macro(configure_onnx)
     message(STATUS "Configuring onnx...")
     set(DAQ_ONNX_NAMESPACE onnx_daq)
     
-    set(ONNX_NAMESPACE ${DAQ_ONNX_NAMESPACE})
+    set(ONNX_NAMESPACE ${DAQ_ONNX_NAMESPACE} CACHE STRING "onnx namespace")
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/onnx)
     # Since https://github.com/onnx/onnx/pull/1318 is merged, we don't need to set it manually
     # target_compile_definitions(onnx
