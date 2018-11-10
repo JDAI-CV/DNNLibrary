c1584eb30d3fc28390b7125e1ca20bf160bcaa61
daquexian <daquexian566@gmail.com>
2018-11-10 16:39:33 +0800

    Disable gflags in glog

--- cmake/glog.cmake
+++ cmake/glog.cmake
@@ -1,5 +1,6 @@
 macro(configure_glog)
     message(STATUS "Configureing glog...")
     option(BUILD_TESTING "" OFF)
+    option(WITH_GFLAGS "" OFF)
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/glog)
 endmacro()
