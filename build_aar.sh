0151b829255b6420487a756acd9c4a390931d2a6
daquexian <daquexian566@gmail.com>
2019-02-01 10:05:04 +0800

    cmake own ndk support is broken for ndk r19 (#31)
    * cmake own ndk support is broken for ndk r19
    
    * create jniLibs/arm64-v8a if not exist
    
    * Fix wrong ndk path

--- build_aar.sh
+++ build_aar.sh
@@ -7,6 +7,7 @@ rm -rf ${JNI_BUILD_DIR} && mkdir ${JNI_BUILD_DIR} && pushd ${JNI_BUILD_DIR}
 cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_TOOLCHAIN_FILE=${MY_ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake -DANDROID_CPP_FEATURES=exceptions -DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DDNN_BUILD_JNI=ON -DDNN_BUILD_SHARED_LIBS=ON -DDNN_BUILD_BIN=OFF ..
 cmake --build . -- "-j$(nproc)"
 popd
+mkdir -p ./android_aar/dnnlibrary/src/main/jniLibs/arm64-v8a
 cp ${JNI_BUILD_DIR}/dnnlibrary/libdnn-jni.so ./android_aar/dnnlibrary/src/main/jniLibs/arm64-v8a/libdaq-jni.so
 
 # Increase version code and update version name
