ef3382620197ab33dacd486ccf72cb30d99a2d4f
daquexian <daquexian566@gmail.com>
2019-04-11 18:53:08 +0800

    Fix bug in publish aar script

--- scripts/publish_aar_to_jcenter.sh
+++ scripts/publish_aar_to_jcenter.sh
@@ -5,4 +5,6 @@ set -e
 
 MY_ANDROID_HOME="${ANDROID_HOME:-$HOME/Android/Sdk}"
 echo "Publishing.."
+pushd android_aar
 ANDROID_HOME=$MY_ANDROID_HOME ./gradlew bintrayUpload -PbintrayUser=daquexian566 -PbintrayKey=$BINTRAY_KEY -PdryRun=false
+popd
