1ef0148dd1819866b72e8efe71d492df1e062f60
daquexian <daquexian566@gmail.com>
2019-04-16 09:26:57 +0800

    Only set ONNX_BUILD_MAIN_LIB to onnx_proto

--- cmake/onnx.cmake
+++ cmake/onnx.cmake
@@ -19,9 +19,9 @@ macro(configure_onnx)
         file(WRITE ${ONNX_CMAKELISTS} "${content}")
     endif()
     set(ONNX_BUILD_MAIN_LIB ON)
-    add_compile_definitions(ONNX_BUILD_MAIN_LIB)
     set(ONNX_NAMESPACE ${DAQ_ONNX_NAMESPACE} CACHE STRING "onnx namespace")
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/onnx)
+    target_compile_definitions(onnx_proto PRIVATE ONNX_BUILD_MAIN_LIB)
     # Since https://github.com/onnx/onnx/pull/1318 is merged, we don't need to set it manually
     # target_compile_definitions(onnx
     # PUBLIC
