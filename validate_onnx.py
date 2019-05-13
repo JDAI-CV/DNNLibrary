f2fff56faf7fc4edb48a9a94185c6bd673b46b36
daquexian <daquexian566@gmail.com>
2019-05-13 14:41:42 +0800

    A more powerful validate_onnx.py. Only dir and dnn_retrieve_result is needed

--- validate_onnx.py
+++ validate_onnx.py
@@ -48,9 +48,8 @@ def run(input_arrs, daq, dnn_retrieve_result, quant_input=False, quant_output=Fa
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Test onnx model on nnapi')
-    parser.add_argument('onnx', type=str, help='onnx model file')
+    parser.add_argument('dir', type=str, help='onnx model file')
     parser.add_argument('dnn_retrieve_result', type=str, help='dnn_retrieve_result binary file')
-    parser.add_argument('test_data_dir', type=str, help='e.g. test_data_set_0')
     parser.add_argument('--onnx2daq', type=str, help='onnx2daq binary file')
     parser.add_argument('--table_file', type=str, help='table file for 8-bit quantization', default='')
     parser.add_argument('--quant_input', help='whether the input is quant8', action='store_true')
@@ -58,36 +57,41 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # Load inputs
-    inputs = []
-    inputs_num = len(glob.glob(os.path.join(args.test_data_dir, 'input_*.pb')))
-    for i in range(inputs_num):
-        input_file = os.path.join(args.test_data_dir, 'input_{}.pb'.format(i))
-        tensor = onnx.TensorProto()
-        with open(input_file, 'rb') as f:
-            tensor.ParseFromString(f.read())
-        np_arr = numpy_helper.to_array(tensor)
-        if args.quant_input:
-            np_arr = np_arr.astype(np.uint8)
-        inputs.append(np_arr)
+    onnx_model = glob.glob(os.path.join(args.dir, '*.onnx'))
+    assert len(onnx_model) == 1
+    onnx_model = onnx_model[0]
+    data_dirs = glob.glob(os.path.join(args.dir, 'test_data_set_*'))
 
-    # Load reference outputs
-    ref_outputs = []
-    ref_outputs_num = len(glob.glob(os.path.join(args.test_data_dir, 'output_*.pb')))
-    for i in range(ref_outputs_num):
-        output_file = os.path.join(args.test_data_dir, 'output_{}.pb'.format(i))
-        tensor = onnx.TensorProto()
-        with open(output_file, 'rb') as f:
-            tensor.ParseFromString(f.read())
-        ref_outputs.append(numpy_helper.to_array(tensor))
+    for data_dir in data_dirs:
+        # Load inputs
+        inputs = []
+        inputs_num = len(glob.glob(os.path.join(data_dir, 'input_*.pb')))
+        for i in range(inputs_num):
+            input_file = os.path.join(data_dir, 'input_{}.pb'.format(i))
+            tensor = onnx.TensorProto()
+            with open(input_file, 'rb') as f:
+                tensor.ParseFromString(f.read())
+            np_arr = numpy_helper.to_array(tensor)
+            if args.quant_input:
+                np_arr = np_arr.astype(np.uint8)
+            inputs.append(np_arr)
 
-    if args.onnx2daq is None:
-        model = args.onnx
-    else:
-        model = "temp.daq"
-        convert(args.onnx2daq, args.onnx, model, args.table_file)
-    os.system("adb push {} /data/local/tmp/".format(model))
-    for i in range(inputs_num):
+        # Load reference outputs
+        ref_outputs = []
+        ref_outputs_num = len(glob.glob(os.path.join(data_dir, 'output_*.pb')))
+        for i in range(ref_outputs_num):
+            output_file = os.path.join(data_dir, 'output_{}.pb'.format(i))
+            tensor = onnx.TensorProto()
+            with open(output_file, 'rb') as f:
+                tensor.ParseFromString(f.read())
+            ref_outputs.append(numpy_helper.to_array(tensor))
+
+        if args.onnx2daq is None:
+            model = onnx_model
+        else:
+            model = "temp.daq"
+            convert(args.onnx2daq, onnx_model, model, args.table_file)
+        os.system("adb push {} /data/local/tmp/".format(model))
         actual = run(inputs, model, args.dnn_retrieve_result, args.quant_input, args.quant_output)
         expected = ref_outputs[i].flatten()
 
@@ -95,9 +99,9 @@ if __name__ == '__main__':
         try:
             print("Max relative diff: {}".format(np.max(np.abs(expected - actual) / expected)))
             np.testing.assert_array_almost_equal(expected, actual, decimal=3)
-            print('No.{} in {} passed'.format(i, args.test_data_dir))
+            print('{} passed'.format(os.path.basename(data_dir)))
         except (AssertionError, ValueError) as e:
-            print('No.{} in {} failed'.format(i, args.test_data_dir))
+            print('{} failed'.format(os.path.basename(data_dir)))
             print(str(e))
             print(expected)
             print('-----')
