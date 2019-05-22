3d8fc1e90f642f4d25643f94e03b520e3a689412
daquexian <daquexian566@gmail.com>
2019-05-22 18:04:03 +0800

    Build python only when DNN_BUILD_PYTHON is on, set PIC to on and misc changes

--- tools/onnx2daq/pywrapper.cpp
+++ tools/onnx2daq/pywrapper.cpp
@@ -8,14 +8,8 @@ void convert(const std::string &model_str,
                             const std::string &filepath,
                             const css &table_file="") {
 
-    ONNX_NAMESPACE::ModelProto model_proto;
-    bool ret = model_proto.ParseFromString(model_str);
-    if (!ret) {
-        throw std::invalid_argument("Read protobuf string failed");
-    }
     OnnxConverter converter;
-    converter.Convert(model_proto, filepath, table_file);
-    google::protobuf::ShutdownProtobufLibrary();
+    converter.Convert(model_str, filepath, table_file);
 }
 
 PYBIND11_MODULE(_onnx2daq, m) {
