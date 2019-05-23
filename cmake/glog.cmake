9b428af946e9784212c797f27d5baf5a1f102fc1
daquexian <daquexian566@gmail.com>
2019-05-23 21:16:11 +0800

    Avoid name conflict in glog.cmake. BUILD_TESTING is conflicted with eigen in onnxruntime

--- cmake/glog.cmake
+++ cmake/glog.cmake
@@ -1,6 +1,13 @@
 macro(configure_glog)
     message(STATUS "Configureing glog...")
-    option(BUILD_TESTING "" OFF)
-    option(WITH_GFLAGS "" OFF)
+
+    set(TEMP_BUILD_TESTING ${BUILD_TESTING})
+    set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+    set(TEMP_WITH_GFLAGS ${WITH_GFLAGS})
+    set(WITH_GFLAGS OFF CACHE BOOL "" FORCE)
+
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/glog)
+
+    set(BUILD_TESTING ${TEMP_BUILD_TESTING} CACHE BOOL "" FORCE)
+    set(WITH_GFLAGS ${TEMP_WITH_GFLAGS} CACHE BOOL "" FORCE)
 endmacro()
